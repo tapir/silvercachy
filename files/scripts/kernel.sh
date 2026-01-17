55bafc2f470b9bd3244911b4305cd0ff386634c2
Cosku Bas <cosku.bas@gmail.com>
2026-01-17 18:04:26 +0100

    remove legacy code

--- files/scripts/kernel.sh
+++ files/scripts/kernel.sh
@@ -4,16 +4,8 @@ set -ouex pipefail
 
 # Remove the kernel files installed by BlueBuild
 for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
-    rpm --erase $pkg --nodeps
+    rpm --erase --nodeps $pkg
 done
-
-# We have to temporarily disable these otherwise build fails
-cd /usr/lib/kernel/install.d \
-    && mv 05-rpmostree.install 05-rpmostree.install.bak \
-    && mv 50-dracut.install 50-dracut.install.bak \
-    && printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install \
-    && printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install \
-    && chmod +x  05-rpmostree.install 50-dracut.install
 
 # Install CachyOS kernel packages
 dnf -y copr enable bieszczaders/kernel-cachyos-lto
@@ -24,8 +16,3 @@ dnf install -y --setopt=install_weak_deps=False \
     kernel-cachyos-lto-modules \
     kernel-cachyos-lto-nvidia-open 
 dnf -y copr remove bieszczaders/kernel-cachyos-lto
-
-# Restore kernel install files
-mv -f 05-rpmostree.install.bak 05-rpmostree.install \
-    && mv -f 50-dracut.install.bak 50-dracut.install \
-    && cd -
